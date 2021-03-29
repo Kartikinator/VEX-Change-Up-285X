236e4373ae0a0b082e80ee0dbe569b64fe42bdea
sukahotfire <59985488+sukahotfire@users.noreply.github.com>
2021-03-29 12:03:20 -0500

    merged code from devices into main

--- src/main.cpp
+++ src/main.cpp
@@ -49,6 +49,11 @@ pros::Motor indexer (INDEXER);
 pros::Motor left_intake (LEFT_INTAKE);
 pros::Motor right_intake (RIGHT_INTAKE, true);
 
+pros::Motor drive_b_l(DRIVE_BACK_LEFT);
+pros::Motor drive_b_r(DRIVE_BACK_RIGHT);
+pros::Motor drive_f_l(DRIVE_FRONT_LEFT);
+pros::Motor drive_f_r(DRIVE_FRONT_RIGHT);
+
 pros::ADIAnalogIn limit_switch ('B');
 
 pros::ADIAnalogIn bumper ('C');
@@ -131,35 +136,110 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
+double move_kP = 0;
+double move_kI = 0;
+double move_kD = 0;
+
+double move_Error = 0;
+double move_lastError = 0;
+double move_intg = 0;
+double move_deriv = 0;
+double move_totalError =0;
+
+bool resetencoders = false;
+bool isPIDon = true;
+
+//use inches for coords pls
+void moveBot(int xcor, int ycor){
+  //Vertical movement
+  double magn = sqrt(xcor^2+ycor^2);
+  double headg = atan2 (ycor,xcor);
+
+  while (isPIDon){
+    if(resetencoders){
+      resetencoders = false;
+      leftencoder.reset();
+      rightencoder.reset();
+    }
+    /////////// DISTANCE PID
+    double avg_enc_val = (leftencoder.get()*41.669+rightencoder.get()*41.669)/2;
+
+    //Proportional
+    move_Error = magn-avg_enc_val;
+
+    //Derivative
+    move_deriv = move_Error-move_lastError;
+
+    //Integral
+    if(move_Error != 0){
+    move_totalError += move_Error;
+  } else {
+    move_totalError = 0;
+  }
+
+    move_lastError = move_Error;
+
+    double mag_sum = move_kP*move_Error + move_kI*move_totalError + move_kD*move_deriv;
+
+
+    //double updated_x_cor = mag_sum*sin(heading);
+    //double updated_y_cor = mag_sum*cos(heading);
+
+
+    drive_b_l.move_voltage((int)(sin(headg-pi/4)*mag_sum)+0.5);
+    drive_b_r.move_voltage((int)(sin(headg+pi/4)*mag_sum)+0.5);
+    drive_f_l.move_voltage((int)(sin(headg+pi/4)*mag_sum)+0.5);
+    drive_f_r.move_voltage((int)(sin(headg-pi/4)*mag_sum)+0.5);
+
+    pros::delay(20);
+}
+
+}
+
+double turn_kP = 0;
+double turn_kI = 0;
+double turn_kD = 0;
+
+double turn_Error = 0;
+double turn_lastError = 0;
+double turn_intg = 0;
+double turn_deriv = 0;
+double turn_totalError = 0;
+
+
+//Use degrees
+void turnBot(int angle){
+
+    double deltaL = leftencoder.get()*41.669;
+    double deltaR = rightencoder.get()*41.669;
+    double track_wheel_dist = 7.25+7.25; //MEASURE THESE ON BOT
+
+    double current_heading = (deltaL+deltaR)/track_wheel_dist;
+    double heading_deg = current_heading*180/pi;
+
+    //turn PID
+
+    //Proportion
+    turn_Error = angle-heading_deg;
+    //Derivative
+    turn_deriv = turn_Error-turn_lastError;
+    //Integral
+    if(turn_Error != 0){
+      turn_totalError += turn_Error;
+    }else{
+      move_totalError = 0;
+    }
+
+    turn_lastError = turn_Error;
+
+    double angle_sum = turn_kP*turn_Error + turn_kI*turn_totalError + turn_kD*turn_deriv;
+
+
+}
 
 void autonomous() {
-		// Declaring Motors ---
-		// pros::Motor TOP_RIGHT (12, true);
-		// pros::Motor TOP_LEFT (DRIVE_FRONT_LEFT);
-		// pros::Motor BACK_RIGHT (11, true);
-		// pros::Motor BACK_LEFT (DRIVE_BACK_LEFT);
-
-		//Drive Wheels
-		// okapi::MotorGroup left({DRIVE_FRONT_LEFT, DRIVE_BACK_LEFT});
-		// okapi::MotorGroup right({DRIVE_FRONT_RIGHT, DRIVE_BACK_RIGHT});
-
-		// Autonomous -----
-
-		// Deploy Hood
-
-		indexer.move_velocity(200);
-		pros::delay(500);
-		indexer.move_velocity(0);
-
-		profileController->generatePath({
-			{0_ft, 0_ft, 0_deg},
-			{3.05_ft, 0_ft, 0_deg}},
-			"straight1"
-		);
-
-
-
-
+	moveBot(1,1);
+	turnBot(90);
 }
 
 /*
