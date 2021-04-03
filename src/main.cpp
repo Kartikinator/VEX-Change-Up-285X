4a137802b75ee9214e6165c135ea5968d902c3aa
Kartikinator <kartikisnice@gmail.com>
2021-04-03 17:08:10 -0500

    better drive

--- src/main.cpp
+++ src/main.cpp
@@ -138,33 +138,221 @@ void competition_initialize() {}
  * from where it left off.
  */
 
-void autonomous() {
+ double move_kP = 0;
+ double move_kI = 0;
+ double move_kD = 0;
 
+ double move_Error = 0;
+ double move_lastError = 0;
+ double move_intg = 0;
+ double move_deriv = 0;
+ double move_totalError =0;
+
+ bool resetencoders = false;
+ bool isPIDon = true;
+
+ //use inches for coords pls
+ void moveBot(int xcor, int ycor){
+   //Vertical movement
+   double magn = sqrt(xcor^2+ycor^2);
+   double headg = atan2 (ycor,xcor);
+
+   while (isPIDon){
+     if(resetencoders){
+       resetencoders = false;
+       leftencoder.reset();
+       rightencoder.reset();
+     }
+     /////////// DISTANCE PID
+     double avg_enc_val = (leftencoder.get()*41.669+rightencoder.get()*41.669)/2;
+
+     //Proportional
+     move_Error = magn-avg_enc_val;
+
+     //Derivative
+     move_deriv = move_Error-move_lastError;
+
+     //Integral
+     if(move_Error != 0){
+     move_totalError += move_Error;
+   } else {
+     move_totalError = 0;
+   }
+
+     move_lastError = move_Error;
+
+     double mag_sum = move_kP*move_Error + move_kI*move_totalError + move_kD*move_deriv;
+
+
+     //double updated_x_cor = mag_sum*sin(heading);
+     //double updated_y_cor = mag_sum*cos(heading);
+
+
+     drive_b_l.move_voltage((int)(sin(headg-pi/4)*mag_sum)+0.5);
+     drive_b_r.move_voltage((int)(sin(headg+pi/4)*mag_sum)+0.5);
+     drive_f_l.move_voltage((int)(sin(headg+pi/4)*mag_sum)+0.5);
+     drive_f_r.move_voltage((int)(sin(headg-pi/4)*mag_sum)+0.5);
+
+     pros::delay(20);
+ }
+
+ }
+
+ double turn_kP = 0;
+ double turn_kI = 0;
+ double turn_kD = 0;
+
+ double turn_Error = 0;
+ double turn_lastError = 0;
+ double turn_intg = 0;
+ double turn_deriv = 0;
+ double turn_totalError = 0;
+
+
+ //Use degrees
+ void turnBot(int angle){
+
+     double deltaL = leftencoder.get()*41.669;
+     double deltaR = rightencoder.get()*41.669;
+     double track_wheel_dist = 7.25+7.25; //MEASURE THESE ON BOT
+
+     double current_heading = (deltaL+deltaR)/track_wheel_dist;
+     double heading_deg = current_heading*180/pi;
+
+     //turn PID
+
+     //Proportion
+     turn_Error = angle-heading_deg;
+     //Derivative
+     turn_deriv = turn_Error-turn_lastError;
+     //Integral
+     if(turn_Error != 0){
+       turn_totalError += turn_Error;
+     }else{
+       move_totalError = 0;
+     }
+
+     turn_lastError = turn_Error;
+
+     double angle_sum = turn_kP*turn_Error + turn_kI*turn_totalError + turn_kD*turn_deriv;
+
+
+ }
+
+void autonomous() {
+	xModel->setBrakeMode(AbstractMotor::brakeMode::brake);
 
 		xModel->strafe(50);
-		pros::delay(500);
+		pros::delay(850);
 		xModel->strafe(0);
 
 		pros::delay(100);
 
 		xModel->rotate(50);
-		pros::delay(200);
+		pros::delay(400);
 		xModel->rotate(0);
+
+		// Approach first goal
+
+		left_intake.move_velocity(-200);
+		right_intake.move_velocity(-200);
+
+		pros::delay(300);
+
+		left_intake.move_velocity(200);
+		right_intake.move_velocity(200);
+		main_intake.move_velocity(200);
+		indexer.move_velocity(-200);
 
 		xModel->forward(50);
 		pros::delay(1000);
 		xModel->forward(0);
 
+		pros::delay(1000);
 
+		// Depart from first goal
 
-		profileController->generatePath({
-			{0_ft, 0_ft, 0_deg},
-			{0_ft, 2_ft, 0_deg}},
-			"straight1"
-		);
+		left_intake.move_velocity(0);
+		right_intake.move_velocity(0);
+		main_intake.move_velocity(0);
+		indexer.move_velocity(0);
 
-		profileController->setTarget("straight1", true);
-		profileController->waitUntilSettled();
+		xModel->forward(-50);
+		pros::delay(600);
+		xModel->forward(0);
+
+		xModel->rotate(50);
+		pros::delay(200);
+		xModel->rotate(0);
+
+		xModel->strafe(-50);
+		pros::delay(950);
+		xModel->strafe(0);
+
+		//Approach second goal
+
+		xModel->forward(50);
+		pros::delay(700);
+		xModel->forward(0);
+
+		left_intake.move_velocity(200);
+		right_intake.move_velocity(200);
+		main_intake.move_velocity(200);
+		indexer.move_velocity(-200);
+
+		pros::delay(1000);
+
+		// Depart from second goal
+
+		left_intake.move_velocity(0);
+		right_intake.move_velocity(0);
+		main_intake.move_velocity(0);
+		indexer.move_velocity(0);
+
+		xModel->forward(-50);
+		pros::delay(700);
+		xModel->forward(0);
+
+		xModel->strafe(-50);
+		pros::delay(900);
+		xModel->strafe(0);
+
+		xModel->rotate(50);
+		pros::delay(200);
+		xModel->rotate(0);
+
+		// Approach third goal
+
+		left_intake.move_velocity(200);
+		right_intake.move_velocity(200);
+		main_intake.move_velocity(200);
+		indexer.move_velocity(-200);
+
+		xModel->forward(50);
+		pros::delay(1000);
+		xModel->forward(0);
+
+		pros::delay(1000);
+
+		left_intake.move_velocity(0);
+		right_intake.move_velocity(0);
+		main_intake.move_velocity(0);
+		indexer.move_velocity(0);
+
+		xModel->forward(-50);
+		pros::delay(1000);
+		xModel->forward(0);
+		//
+		//
+		//
+		// profileController->generatePath({
+		// 	{0_ft, 0_ft, 0_deg},
+		// 	{2_ft, 0_ft, 0_deg}},
+		// 	"straight1"
+		// );
+		//
+		// profileController->setTarget("straight1", true);
+		// profileController->waitUntilSettled();
 
 		// indexer.move_velocity(200);
 		// pros::delay(500);
@@ -252,8 +440,8 @@ void autonomous() {
  		else if (right2.isPressed()) {
 
  		if (limit_switch.get_value() >11) {
- 		main_intake.move_velocity(150);
- 		indexer.move_velocity(-50);
+ 		main_intake.move_velocity(200);
+ 		indexer.move_velocity(-100);
  		left_intake.move_velocity(200);
  		right_intake.move_velocity(200);
  	} else {
